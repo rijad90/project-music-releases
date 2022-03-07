86bfa98a60a9256d01207780f8ed49ea3faa61cc
rijad90 <rijad.i@hotmail.com>
2022-03-07 17:29:34 +0100

    first draft, added artist name

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import data from './data.json'
+//import {Article} from './components/Article'
+//import {Person} from './components/Person'
+//import {Album} from 'components/Album'
+//import {Album} from './components/Album'
 
 console.log(data)
 
@@ -7,7 +11,51 @@ console.log(data)
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
-    </div>
+    {data.albums.items.map(item => {
+      return <div key={item.id}>
+        {item.artists.map(item => {
+          return <span key={item.id}>{item.name} </span>
+        })}
+      </div>;
+    })}
+  </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<Article/>
+<Article/>  
+<Person name="Rijad"/>
+<Person name="Rijad1"/>
+<Person name="Rijad2"/> 
+{data.map((album) =>   { 
+      return <Album name = {album.name} />
+      })}
+
+      <div>
+      <Album name="test"/>
+      <Article/>
+      <Person name="Rijad"/>
+    </div>
+*/
---
+++ code/src/components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Person = props => {
+    return (
+        <div>
+        Hello {props.name} {props.title}
+        </div>
+    )
+}
+
